2a1ad2b85209ff194c6a752062557d78b240a587
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-08 21:05:26 -0800

    Cache seen RMP search results

--- lib/rmp/index.ts
+++ lib/rmp/index.ts
@@ -18,8 +18,14 @@ function normalizeName(name: string) {
     return name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
 }
 
+const cachedTeachers = new Map<string, TeacherSearchResult>();
+
 export async function searchTeacher(fullName: string): Promise<TeacherSearchResult | null> {
     fullName = normalizeName(decodeURIComponent(fullName));
+
+    const cachedResult = cachedTeachers.get(fullName);
+    if (cachedResult)
+        return cachedResult;
 
     const searchResults: TeacherSearchResults = await client.request(searchTeacherQuery, {
         includeSchoolFilter: true,
@@ -34,12 +40,18 @@ export async function searchTeacher(fullName: string): Promise<TeacherSearchResu
     const edges = searchResults.search.teachers.edges;
     if (edges.length === 0) return null;
 
+    edges.forEach(edge => {
+        const cacheName = normalizeName(edge.node.firstName + " " + edge.node.lastName);
+        cachedTeachers.set(cacheName, edge.node);
+    });
+
     const nameSegments = fullName.split(" ");
     const firstProfessor = edges[0].node;
     if (!normalizeName(firstProfessor.firstName).startsWith(nameSegments[0]))
         return null;
     if (nameSegments.length > 1 && !normalizeName(firstProfessor.lastName).endsWith(nameSegments[nameSegments.length - 1]))
         return null;
+    cachedTeachers.set(fullName, firstProfessor);
     return firstProfessor;
 }
 
