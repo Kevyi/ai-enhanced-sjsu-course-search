51e5123a9c25ee6099472b181c6373b06fdcae25
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-12 03:12:42 -0700

    Optimize sections scraping

--- lib/sjsu/index.ts
+++ lib/sjsu/index.ts
@@ -1,19 +1,21 @@
 "use server";
 
-import { JSDOM } from "jsdom";
+import {HTMLAnchorElement, HTMLTableCellElement, Window} from "happy-dom";
 import {Section} from "@/lib/sjsu/types";
+
+type TableHeadingProcessor = {name: keyof Section} | {process: (section: Partial<Section>, td: HTMLTableCellElement) => void};
 
 const SCHEDULE_URL =
     "https://www.sjsu.edu/classes/schedules/{season}-{year}.php";
 
 const TABLE_HEADING_PROPS: {
-    [heading: string]: {name: keyof Section} | {process: (section: Partial<Section>, td: HTMLTableCellElement) => void}
+    [heading: string]: TableHeadingProcessor
 } = {
     Section: {
         process: (section, td) => {
             section.section = td.textContent!;
             // PSYC 196 courses do not have a section url
-            section.section_url = td.querySelector("a")?.href;
+            section.section_url = (td.firstElementChild as HTMLAnchorElement)?.href;
         },
     },
     "Class Number": { name: "class_number" },
@@ -27,7 +29,7 @@ const TABLE_HEADING_PROPS: {
     Instructor: {
         process: (section, td) => {
             section.instructor = td.textContent!;
-            section.instructor_email = td.querySelector("a")?.href.replace("mailto:", "");
+            section.instructor_email = (td.firstElementChild as HTMLAnchorElement)?.href.replace("mailto:", "");
         },
     },
     Location: { name: "location" },
@@ -37,30 +39,35 @@ const TABLE_HEADING_PROPS: {
 };
 
 export async function getSections(season: "spring" | "summer" | "fall" | "winter", year: number) {
-    const {
-        window: { document },
-    } = await JSDOM.fromURL(
-        SCHEDULE_URL.replaceAll("{season}", season).replaceAll("{year}", year.toString())
-    );
-    const scheduleTable = document.querySelector("#classSchedule")!;
-    const tableColumnProcessors = [
-        ...scheduleTable.querySelectorAll("thead tr th"),
-    ].map((th) => {
-        return TABLE_HEADING_PROPS[th.textContent!];
-    });
+    const html = await (await fetch(SCHEDULE_URL.replaceAll("{season}", season).replaceAll("{year}", year.toString()))).text();
+    const window  = new Window();
+    const document = window.document;
+    document.body.innerHTML = html;
 
-    return [...scheduleTable.querySelectorAll("tbody tr")].map((tr) => {
+    const scheduleTable = document.querySelector("#classSchedule")!;
+    const tableColumnProcessors: TableHeadingProcessor[] = [];
+    for (let th = scheduleTable.querySelector("thead tr th"); th != null; th = th.nextElementSibling) {
+        tableColumnProcessors.push(TABLE_HEADING_PROPS[th.textContent!]);
+    }
+
+    const sections: Section[] = [];
+    for (let tr = scheduleTable.querySelector("tbody tr"); tr != null; tr = tr.nextElementSibling) {
         const section: Partial<Section> = {};
-        [...tr.querySelectorAll("td")].forEach((td, i) => {
+        let i = 0;
+        for (let td = tr.firstElementChild as HTMLTableCellElement; td != null; td = td.nextElementSibling as HTMLTableCellElement) {
             const column = tableColumnProcessors[i];
-            if (!column) return;
+            i++;
 
             if ("name" in column) {
                 section[column.name] = td.textContent!.trim();
             } else if ("process" in column) {
                 column.process(section, td);
             }
-        });
-        return section as Section;
-    });
+        }
+
+        sections.push(section as Section);
+    }
+
+    window.close();
+    return sections;
 }
