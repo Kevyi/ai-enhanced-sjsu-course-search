bd798ef7e882fa4e964d294afe6b3c2c536d5fc4
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-21 18:21:46 -0700

    Add option to dump sections to MongoDB

--- scripts/dump_sections.ts
+++ scripts/dump_sections.ts
@@ -1,14 +1,59 @@
+import "dotenv/config";
 import { getSections } from "@/lib/sjsu";
 import { writeFile } from "fs/promises";
+import { AnyBulkWriteOperation, MongoClient } from "mongodb";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-(async() => {
-    const season = process.argv[2] ?? "spring";
-    if (season !== "spring" && season !== "summer" && season !== "fall" && season !== "winter") {
-        console.error("Unknown season: " + season);
-        return;
-    }
-    const year = process.argv[3] ? parseInt(process.argv[3]) : (new Date()).getFullYear();
+(async () => {
+  const { season, year, mode } = await yargs(hideBin(process.argv))
+    .options({
+      season: {
+        choices: ["spring", "summer", "fall", "winter"] as const,
+        default: "spring" as const,
+      },
+      year: {
+        type: "number",
+        default: new Date().getFullYear(),
+      },
+      mode: {
+        choices: ["json", "mongodb"] as const,
+        default: "json" as const,
+      },
+    })
+    .parse();
 
-    const sections = await getSections(season, year);
-    await writeFile(`sections-${season}-${year}.json`, JSON.stringify(sections, undefined, "\t"), "utf-8");
+  const sections = await getSections(season, year);
+  if (mode === "json") {
+    await writeFile(
+      `sections-${season}-${year}.json`,
+      JSON.stringify(sections, undefined, "\t"),
+      "utf-8"
+    );
+  } else {
+    const client = new MongoClient(process.env.MONGODB_URI!);
+    await client.connect();
+
+    const db = client.db("course_sections");
+    const collection = db.collection(season + "-" + year);
+
+    const updateOperations: AnyBulkWriteOperation[] = sections.map(
+      (section) => {
+        return {
+          updateOne: {
+            filter: {
+              section: section.section,
+            },
+            update: {
+              $set: section,
+            },
+            upsert: true,
+          },
+        };
+      }
+    );
+    await collection.bulkWrite(updateOperations);
+
+    client.close();
+  }
 })();
