ea921ccd82a5d2b508adf04cfa4ae9680e707999
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-03-27 19:08:40 -0700

    Scrape all available semesters if no specific semester provided

--- scripts/dump_sections.ts
+++ scripts/dump_sections.ts
@@ -1,20 +1,19 @@
 import "dotenv/config";
-import { getSections } from "@/lib/sjsu";
+import { getAvailableSemesters, getSections } from "@/lib/sjsu";
 import { writeFile } from "fs/promises";
 import { AnyBulkWriteOperation, MongoClient } from "mongodb";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import { Season } from "@/lib/sjsu/types";
 
 (async () => {
   const { season, year, mode } = await yargs(hideBin(process.argv))
     .options({
       season: {
         choices: ["spring", "summer", "fall", "winter"] as const,
-        default: "spring" as const,
       },
       year: {
         type: "number",
-        default: new Date().getFullYear(),
       },
       mode: {
         choices: ["json", "mongodb"] as const,
@@ -23,39 +22,48 @@ import { hideBin } from "yargs/helpers";
     })
     .parse();
 
-  const sections = await getSections(season, year);
-  if (mode === "json") {
-    await writeFile(
-      `sections-${season}-${year}.json`,
-      JSON.stringify(sections, undefined, "\t"),
-      "utf-8"
-    );
+  const semesters: [Season, number][] = [];
+  if (season && year) {
+    semesters.push([season, year]);
   } else {
-    const client = new MongoClient(process.env.MONGODB_URI!);
-    await client.connect();
+    semesters.push(...(await getAvailableSemesters()));
+  }
 
-    const db = client.db("cmpe151");
-    const collection = db.collection("course_sections");
+  for (const [season, year] of semesters) {
+    const sections = await getSections(season, year);
+    if (mode === "json") {
+      await writeFile(
+        `sections-${season}-${year}.json`,
+        JSON.stringify(sections, undefined, "\t"),
+        "utf-8"
+      );
+    } else {
+      const client = new MongoClient(process.env.MONGODB_URI!);
+      await client.connect();
 
-    const updateOperations: AnyBulkWriteOperation[] = sections.map(
-      (section) => {
-        return {
-          updateOne: {
-            filter: {
-              class_number: section.class_number,
-              season,
-              year
+      const db = client.db("cmpe151");
+      const collection = db.collection("course_sections");
+
+      const updateOperations: AnyBulkWriteOperation[] = sections.map(
+        (section) => {
+          return {
+            updateOne: {
+              filter: {
+                class_number: section.class_number,
+                season,
+                year,
+              },
+              update: {
+                $set: { ...section, season, year },
+              },
+              upsert: true,
             },
-            update: {
-              $set: {...section, season, year},
-            },
-            upsert: true,
-          },
-        };
-      }
-    );
-    await collection.bulkWrite(updateOperations);
+          };
+        }
+      );
+      await collection.bulkWrite(updateOperations);
 
-    client.close();
+      client.close();
+    }
   }
 })();
