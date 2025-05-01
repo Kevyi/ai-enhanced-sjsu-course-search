a96a20d1a8e89c3fd4cfdd27e5f170f035a1085d
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-04-30 23:26:04 -0700

    Remove useless dependency on rmp.json file

--- app/courseTest/page.tsx
+++ app/courseTest/page.tsx
@@ -2,13 +2,10 @@
 
 import CourseTable from "@/components/courseComponents/CourseTable"
 import { getCachedAvailableSemesters, getCachedSections } from "@/lib/sjsu/cached";
-import _rmpData from "../../public/rmp.json";
-import { RMPInfo } from "@/lib/rmp/type";
 import Navbar from "@/components/Navbar"
 
 export default async function TestPage(){
     const sections = (await getCachedSections("spring", 2025)).slice(0, 100);
-    const rmpData: RMPInfo = _rmpData as RMPInfo;
 
 
     return <>
