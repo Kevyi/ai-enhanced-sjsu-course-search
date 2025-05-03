942a4fd4b11aba19391c716a34ce0a568bb6cca6
NotMyPersonalAccount <kevinzj710@gmail.com>
2025-05-02 17:34:14 -0700

    Accept search params to select semester

--- app/courseTest/page.tsx
+++ app/courseTest/page.tsx
@@ -3,10 +3,13 @@
 import CourseTable from "@/components/courseComponents/CourseTable"
 import { getCachedAvailableSemesters, getCachedSections } from "@/lib/sjsu/cached";
 import Navbar from "@/components/Navbar"
+import { Season } from "@/lib/sjsu/types";
 
-export default async function TestPage(){
-    const sections = (await getCachedSections("spring", 2025));
-    const semesters = (await getCachedAvailableSemesters())
+export default async function TestPage({ searchParams }: { searchParams: Promise<{ season?: string, year?: string }> }){
+    const semesters = (await getCachedAvailableSemesters());
+    const latestSemester = semesters[semesters.length - 1];
+    const { season, year } = await searchParams;
+    const sections = (await getCachedSections(season as Season ?? latestSemester[0], year ? parseInt(year) : latestSemester[1]));
 
     return <>
 
